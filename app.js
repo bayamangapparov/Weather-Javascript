4c4117f5bb6121ced794fa2e3d19b982ca441dd3
Bayaman Gapparov <bayamangapparov@gmail.com>
2019-11-27 21:46:09 -0600

    Added argumetns value

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(
+        ".temperature-description"
+    );
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureTimezone = document.querySelector(".location-timezone");
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             console.log(position);
-        })
-    } else {
-        h1.textContent = "It is does not worlking, Try later!"
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/a802e9fb9582a4d65bd49eb83f0d3918/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data)
+                    const { temperature, summary } = data.currently;
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    temperatureTimezone.textContent = data.timezone;
+                })
+        });
     }
 
 });
