d939f5a17185615ba5a8b2610aa69b249e7e794b
Bayaman Gapparov <bayamangapparov@gmail.com>
2019-11-27 22:28:49 -0600

    Added celsius, setIcon fuction etc

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener('load', () => {
     );
     let temperatureDegree = document.querySelector(".temperature-degree");
     let temperatureTimezone = document.querySelector(".location-timezone");
+    let temperatureSection = document.querySelector(".temperature");
+    let temperatureSpan = document.querySelector(".temperature span");
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -22,12 +24,34 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data)
-                    const { temperature, summary } = data.currently;
+                    const { temperature, summary, icon } = data.currently;
+
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     temperatureTimezone.textContent = data.timezone;
-                })
+
+                    let celsius = (temperature - 32) * (5 / 9);
+
+                    setIcons(icon, document.querySelector(".icon"));
+
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureDegree.textContent = temperature;
+                            temperatureSpan.textContent = "F";
+                        }
+                    });
+                });
         });
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 
 });
